28d1041911a80724c1f96fca91559eb000df5b63
AroZarg <99825574+AroZarg@users.noreply.github.com>
2022-02-27 11:25:22 +0300

    Add files via upload

---
+++ number4.js
@@ -0,0 +1,10 @@
+//Check whether a given number is negative. Print “yes”, if it is negative, print “no”
+//otherwise.
+let a;
+a=prompt('tveq 0 -71 89 tvery')
+if (a<=0){
+    alert('No')
+    
+}else if(a>0){
+    alert('yes')
+}
