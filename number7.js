b42032b4aad375c87e773eff3aa493e33772aa69
AroZarg <99825574+AroZarg@users.noreply.github.com>
2022-02-27 11:26:45 +0300

    Add files via upload

---
+++ number7.js
@@ -0,0 +1,14 @@
+// Given three numbers. Find the maximum one.
+let a
+let b
+let c
+a=prompt('tveq a i arjeqy  ')
+b=prompt('tveq b i arjeqy  ')
+c=prompt('tveq c i arjeqy  ')
+if (a>b&&a>c){
+    alert(a)
+}else if (b>a&&b>c){
+    alert(b)
+}else if (c>a&&c>b){
+    alert(c)
+}
