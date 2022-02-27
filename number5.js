cb9a6d27c1c38ae4e1d559456d319002bdec11c4
AroZarg <99825574+AroZarg@users.noreply.github.com>
2022-02-27 11:25:58 +0300

    Add files via upload

---
+++ number5.js
@@ -0,0 +1,10 @@
+//Given two numbers print 1 if one of them is divisible by the other one, otherwise print 0.
+let a
+let b
+a=prompt('tveq a in hamapatasxan arjeky 3 18 7 tvery ')
+b=prompt('tveq b in hamapatasxan arjeky 14 2 21 tvery ')
+if(a%b===0||b%a===0){
+    alert('1')
+} else {
+    alert('0')
+}
