8340ca1de3b487b4e574ec4f4a89d1960fbbf26c
AroZarg <99825574+AroZarg@users.noreply.github.com>
2022-02-27 11:23:39 +0300

    Add files via upload

---
+++ number1.js
@@ -0,0 +1,40 @@
+//number1 Declare 2 variables a and b, such that a>b, and define values for them. Output their
+let a;
+let c;
+let b;
+a=14;
+b=5;
+c=a+b
+alert(c)
+c=a-b
+alert(c)
+c=a*b
+alert(c)
+c=a/b
+alert(c)
+c=a%b
+alert(c)
+a=99;
+b=11;
+c=a+b
+alert(c)
+c=a-b
+alert(c)
+c=a*b
+alert(c)
+c=a/b
+alert(c)
+c=a%b
+alert(c)
+a=81;
+b=16;
+c=a+b
+alert(c)
+c=a-b
+alert(c)
+c=a*b
+alert(c)
+c=a/b
+alert(c)
+c=a%b
+alert(c)
