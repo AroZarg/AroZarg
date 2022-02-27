8bee15f031bca75cde001a353ea83dc18a080cf0
AroZarg <99825574+AroZarg@users.noreply.github.com>
2022-02-27 11:25:02 +0300

    Add files via upload

---
+++ number3.js
@@ -0,0 +1,15 @@
+//Declare a variable with defined value. Print the last digit of the number.
+let a;
+let b;
+a=78756;
+b=a%10
+alert(b)
+a=-122
+b=a%10
+alert(b)
+a=8
+b=a%10
+alert(b)
+a=0
+b=a%10
+alert(b)
