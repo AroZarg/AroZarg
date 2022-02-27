c6abcf3495571d0cdcb1e74290782e87bc780a9d
AroZarg <99825574+AroZarg@users.noreply.github.com>
2022-02-27 11:26:22 +0300

    Add files via upload

---
+++ number6.js
@@ -0,0 +1,44 @@
+//Given any number between 1 and 12. Print the name of the respective month.
+let a
+a=prompt('nshvats tivy')
+switch(a){
+    case '1':
+        alert('janury')
+        break;
+    case '2':
+        alert('febury')
+        break;
+    case '3':
+        alert('martch')
+        break;
+    case '4':
+        alert("april")
+        break;
+    case '5':
+        alert("may")
+        break;
+    case '6':
+        alert("jun")
+        break;
+    case '7':
+        alert("july")
+        break;
+    case '8':
+        alert("aug")
+        break;
+    case '9': 
+        alert("sept")
+        break;
+    case '10':
+        alert("oct")
+          break;
+    case '11':
+        alert("noy")
+         break;
+    case '12':
+        alert("dec")
+          break;
+    default:
+        alert(a)
+} 
+  
